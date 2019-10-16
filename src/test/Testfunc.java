00607a9532fa4ff8161b643a152187a0c430a94b
Ben Dod <benjdod@live.unc.edu>
2019-10-15 23:05:13 -0400

    tried to flatten it all

--- src/test/Testfunc.java
+++ src/test/Testfunc.java
@@ -6,10 +6,18 @@ import base.token.Tokenizer;
 
 public class Testfunc {
     public static void main(String[] args) {
-        Child c = new Child(new Branch(new Branch(3,2,Op.MINUS),2,Op.DIV));
-        TreeTraverse t = new TreeTraverse(c);
-        Child d = c.getBranch().getLeftChild().getParent();
+        // if you debug, you'll see these are different objects
+        // this is likely due to constructor issues
+        Branch b = new Branch(new Branch(7,new Branch(4,1,Op.PLUS),Op.MINUS),2,Op.DIV);
+        Child c = b.getThisChild();
+        Child d = b.getLeftChild().getParent();
+        Child e = b.getLeftChild().getBranch().getRightChild();
+        Child f = e.getParent();
+        Child g = f.getParent();
+        Child t = b.getLeftChild();
         System.out.println(c == d);
+        System.out.println(b.getLeftChild() == f);
+        System.out.println(c == e);
     }
 
 }
