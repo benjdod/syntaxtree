ac61d06214f627529e53c59e246d51114289bff6
Ben Dod <benjdod@live.unc.edu>
2019-10-15 23:58:37 -0400

    save testfunc

--- src/test/Testfunc.java
+++ src/test/Testfunc.java
@@ -8,16 +8,13 @@ public class Testfunc {
     public static void main(String[] args) {
         // if you debug, you'll see these are different objects
         // this is likely due to constructor issues
-        Branch b = new Branch(new Branch(7,new Branch(4,1,Op.PLUS),Op.MINUS),2,Op.DIV);
-        Child c = b.getThisChild();
-        Child d = b.getLeftChild().getParent();
-        Child e = b.getLeftChild().getBranch().getRightChild();
-        Child f = e.getParent();
-        Child g = f.getParent();
-        Child t = b.getLeftChild();
-        System.out.println(c == d);
-        System.out.println(b.getLeftChild() == f);
-        System.out.println(c == e);
+        Branch b = new Branch(2,3,Op.PLUS);
+        Branch c = new Branch(5,4,Op.MINUS);
+        b.setRight(c);
+        TreeTraverse t = new TreeTraverse(b.getThisChild());
+        t.toRight();
+        t.getCurrent();
+        t.toParent();
     }
 
 }
